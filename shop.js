7dc9308573c2fffaa3f2de387e7eec1edd070cb8
Flichendery <80315730+Flichendery@users.noreply.github.com>
2025-12-09 13:21:37 +0300

    Update product prices and adjust image tag

--- shop.js
+++ shop.js
@@ -1,11 +1,11 @@
 // Список товаров
 const items = [
-    { title: "Thriller Jacket", price: 89, img: "https://m.media-amazon.com/images/I/61a4VkAhOtL._AC_UY1000_.jpg" },
-    { title: "Billie Jean Hat", price: 25, img: "https://i.ebayimg.com/images/g/S7gAAOxy~dNTJEdd/s-l400.jpg" },
-    { title: "Smooth Criminal Suit", price: 120, img: "https://m.media-amazon.com/images/I/71l4Kq2W6uL.jpg" },
-    { title: "MJ T-Shirt Classic", price: 20, img: "https://www.magic-custom.com/21229-large_default/t-shirt-noir-retro-90-s-vintage-bootleg-michael-jackson-big-face.jpg" },
-    { title: "Bad Tour Hoodie", price: 55, img: "https://i.ebayimg.com/images/g/hHoAAOSwuYRg7K4Z/s-l1200.jpg" },
-    { title: "King of Pop Cap", price: 22, img: "https://i.etsystatic.com/17035524/r/il/9208de/3264800174/il_fullxfull.3264800174_obth.jpg" }
+    { title: "Thriller Jacket", price: 6300, img: "https://m.media-amazon.com/images/I/61a4VkAhOtL._AC_UY1000_.jpg" },
+    { title: "Billie Jean Hat", price: 1900, img: "https://i.ebayimg.com/images/g/S7gAAOxy~dNTJEdd/s-l400.jpg" },
+    { title: "Smooth Criminal Suit", price: 15500, img: "https://m.media-amazon.com/images/I/71l4Kq2W6uL.jpg" },
+    { title: "MJ T-Shirt Classic", price: 1400, img: "https://www.magic-custom.com/21229-large_default/t-shirt-noir-retro-90-s-vintage-bootleg-michael-jackson-big-face.jpg" },
+    { title: "Bad Tour Hoodie", price: 5000, img: "https://i.ebayimg.com/images/g/hHoAAOSwuYRg7K4Z/s-l1200.jpg" },
+    { title: "King of Pop Cap", price: 1000, img: "https://i.etsystatic.com/17035524/r/il/9208de/3264800174/il_fullxfull.3264800174_obth.jpg" }
 ];
 
 const container = document.getElementById("products");
@@ -15,7 +15,7 @@ items.forEach(item => {
     container.innerHTML += `
         <div class="product-card">
             <div class="img-wrap">
-                <img src="${item.img}" width="250" alt="${item.title}">
+                <img src="${item.img}" alt="${item.title}"  width="250">
             </div>
             <h3>${item.title}</h3>
             <div class="price">$${item.price}</div>
